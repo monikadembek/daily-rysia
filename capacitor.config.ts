af2011cba11ed430a0ed9b2dbb6824a6e62d9d46
Monika <monika.dembek@gmail.com>
2024-12-11 17:05:44 +0100

    Add android project, add prettier and eslint

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.dailyRysiaApp.ionic',
   appName: 'daily-rysia-app',
   webDir: 'www'
 };
