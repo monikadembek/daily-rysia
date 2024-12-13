55efc0192197a5dc7fadc223b34865ee0b5ccde1
Monika <monika.dembek@gmail.com>
2024-12-13 15:37:32 +0100

    feat: initialize firebase, add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC61SOUmp386Nxo5ry8TMm9HxOa-nw4LVA',
+    authDomain: 'ang-tictactoe.firebaseapp.com',
+    databaseURL: 'https://ang-tictactoe.firebaseio.com',
+    projectId: 'ang-tictactoe',
+    storageBucket: 'ang-tictactoe.firebasestorage.app',
+    messagingSenderId: '680495170100',
+    appId: '1:680495170100:web:a2b2bf6b10491c092e4061',
+    measurementId: 'G-ZTW4DJ5E8W',
+  },
 };
