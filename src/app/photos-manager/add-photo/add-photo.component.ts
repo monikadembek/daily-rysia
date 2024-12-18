060d4aa0a8555aa1e74d289c627c7627dd3c63d7
Monika <monika.dembek@gmail.com>
2024-12-18 14:46:07 +0100

    feat: Add error handing and loading indicatios when photo is being uploaded and stored in firestore

--- src/app/photos-manager/add-photo/add-photo.component.ts
+++ src/app/photos-manager/add-photo/add-photo.component.ts
@@ -1,11 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { IonGrid, IonRow, IonCol, IonInput, IonItem, IonButton } from '@ionic/angular/standalone';
+import {
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonInput,
+  IonItem,
+  IonButton,
+  AlertController,
+  IonLoading,
+} from '@ionic/angular/standalone';
 import { ImagePickerComponent } from '../image-picker/image-picker.component';
 import { UtilsService } from '../services/utils.service';
 import { CloudinaryUploadResponse, PhotoManagerService } from '../services/photo-manager.service';
-import { switchMap } from 'rxjs';
+import { catchError, EMPTY, switchMap } from 'rxjs';
 import { DocumentReference } from '@angular/fire/firestore';
 
 @Component({
@@ -14,6 +23,7 @@ import { DocumentReference } from '@angular/fire/firestore';
   styleUrls: ['./add-photo.component.scss'],
   standalone: true,
   imports: [
+    IonLoading,
     CommonModule,
     ReactiveFormsModule,
     IonGrid,
@@ -32,11 +42,13 @@ export class AddPhotoComponent implements OnInit {
   });
   imageFile: File | null = null;
   showPreview = true;
+  isUploading = false;
 
   constructor(
     private fb: FormBuilder,
     private utilsService: UtilsService,
     private photoManagerService: PhotoManagerService,
+    private alertController: AlertController,
   ) {}
 
   ngOnInit() {}
@@ -53,20 +65,30 @@ export class AddPhotoComponent implements OnInit {
     this.form.patchValue({ photoFile: this.imageFile });
   }
 
-  // TODO:
-  // 1. handle errors from upload and push to firestore
-  // 2. add loaders and possible alers or error messages
   submitForm() {
     if (this.form.invalid) {
       return;
     }
     console.log(this.form);
+    this.isUploading = true;
     this.photoManagerService
       .uploadImageToCloudinary(this.imageFile!)
       .pipe(
+        catchError((error: any) => {
+          console.log('error uploading image: ', error);
+          this.isUploading = false;
+          this.presentAlert('Error occured while uploading image, please try again later');
+          return EMPTY;
+        }),
         switchMap((res: CloudinaryUploadResponse) => {
           console.log('upload image res: ', res);
           return this.photoManagerService.storePhotoInFirestore(this.form.value.caption, res.url);
+        }),
+        catchError((error: any) => {
+          console.log('error storing data in firebase: ', error);
+          this.isUploading = false;
+          this.presentAlert('Error occured while uploading image, please try again later');
+          return EMPTY;
         }),
       )
       .subscribe((doc: DocumentReference) => {
@@ -83,7 +105,17 @@ export class AddPhotoComponent implements OnInit {
           this.form.reset();
           this.imageFile = null;
           this.showPreview = false;
+          this.isUploading = false;
         }
       });
+  }
+
+  private async presentAlert(errorMessage: string): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: errorMessage,
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 }
