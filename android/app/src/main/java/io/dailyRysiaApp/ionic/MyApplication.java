c6a4c299505d0166db3d54d179bd823b03e4f6f4
Monika <monika.dembek@gmail.com>
2024-12-29 19:01:00 +0100

    fix: fix issues related to app not working on android, appID mismatch and missing google services config

---
+++ android/app/src/main/java/io/dailyRysiaApp/ionic/MyApplication.java
@@ -0,0 +1,12 @@
+package io.dailyRysiaApp.ionic; // Make sure this matches your actual package name
+
+import android.app.Application;
+import com.google.firebase.FirebaseApp;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+    }
+}
